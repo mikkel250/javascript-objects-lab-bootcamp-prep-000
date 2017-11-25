822e55915963fede9c666885535707abba54ed9f
Mikkel Ridley <mikkel250@gmail.com>
2017-11-25 20:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object);
   delete newObject[key];
-  object;
+  // object;
   newObject;
   
 }
