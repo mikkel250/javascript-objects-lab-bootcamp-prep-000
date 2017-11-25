a9e01dff851b454665ad4012493b49767b5a79e6
Mikkel Ridley <mikkel250@gmail.com>
2017-11-25 20:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  delete object[key];
+  return object;
 }
