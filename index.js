91657b76ab20cf84cfd343685d4653a9f7915782
Mikkel Ridley <mikkel250@gmail.com>
2017-11-25 20:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var recipes = { a: '1', b: '2' };
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  // return object;
+  Object.assign({newObject}, {key: value});
   return object;
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, {key: value});
+  Object.assign({newObject}, {key: value});
   return object;
 }
